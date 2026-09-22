dfe7e8cb454732c9f0ec1a0c2fe90fb3e41c02f1
semireid97 <semireid97@gmail.com>
2026-09-22 12:47:09 +0300

    Update app name to ME Tekstil Aİ

--- android-app/app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ android-app/app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -19,7 +19,7 @@ class ExampleRobolectricTest {
     fun `read string from context`() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         val appName = context.getString(R.string.app_name)
-        assertEquals("M&E Tekstil Trendyol", appName)
+        assertEquals("ME Tekstil Aİ", appName)
     }
 
     @Test
